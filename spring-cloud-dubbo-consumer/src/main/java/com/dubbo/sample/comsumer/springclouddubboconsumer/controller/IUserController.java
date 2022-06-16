c6f36c957df6e1fb06c420d14e89ffce966796ba
parrypotter <panruihero@163.com>
2022-06-17 01:38:04 +0800

    2022/06/17-springcloud-consumer修正返回

--- spring-cloud-dubbo-consumer/src/main/java/com/dubbo/sample/comsumer/springclouddubboconsumer/controller/IUserController.java
+++ spring-cloud-dubbo-consumer/src/main/java/com/dubbo/sample/comsumer/springclouddubboconsumer/controller/IUserController.java
@@ -1,10 +1,13 @@
 package com.dubbo.sample.comsumer.springclouddubboconsumer.controller;
 
+import com.alibaba.fastjson2.JSON;
 import com.service.IUserService;
 import org.apache.dubbo.config.annotation.Reference;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 /**
  * @author parry
@@ -13,11 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/spring/cloud")
 public class IUserController {
 
-    @Reference(mock = "com.dubbo.sample.comsumer.springclouddubboconsumer.service.MockService", cluster = "failfast")
+    @Reference(mock = "com.dubbo.sample.comsumer.springclouddubboconsumer.service.MockServiceImpl", cluster = "failfast")
     private IUserService iUserService;
 
-    @GetMapping("/start")
-    public String IUserControllerStartTesting() {
-        return iUserService.getVersion("s");
+
+    @PostMapping("/generateToken")
+    public ResponseEntity<String> generateToken(@RequestBody String params) {
+        Map<String, Object> map = JSON.parseObject(params);
+        return new ResponseEntity(iUserService.generateToken(map), HttpStatus.OK);
     }
 }
---
+++ spring-cloud-dubbo-consumer/src/main/java/com/dubbo/sample/comsumer/springclouddubboconsumer/service/MockServiceImpl.java
@@ -0,0 +1,33 @@
+package com.dubbo.sample.comsumer.springclouddubboconsumer.service;
+
+import com.service.IUserService;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author parry
+ */
+public class MockServiceImpl implements IUserService {
+
+
+    @Override
+    public String generateToken(Map<String, Object> map) {
+        return "Mock Service is Loading";
+    }
+
+    @Override
+    public Map<String, Object> validateTokenAndGetClaims(String jwt) throws ExpiredJwtException, UnsupportedJwtException {
+        Map<String, Object> map = new HashMap<>();
+        map.put(jwt, "Mock Service is Loading");
+        return map;
+    }
+
+    @Override
+    public void setHeader(HttpServletResponse response, String jwt) {
+
+    }
+}
